fc5173a4a4d01ae6e211f410448d61330c40bcd1
AndreasDal <guapo_dal@hotmail.com>
2026-03-27 08:39:34 +0100

    Refactor question handling to use structured TOML format and update quiz logic for improved functionality

--- src/quiz.py
+++ src/quiz.py
@@ -8,68 +8,39 @@ except ModuleNotFoundError:
 
 NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
-QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
-
-
-# QUESTIONS = {
-#     "When was the first known use of the word 'quiz'": ["1781", "1771", "1871", "1881"],
-#     "Which built-in function can get information from the user": [
-#         "input",
-#         "get",
-#         "print",
-#         "write",
-#     ],
-#     "Which keyword do you use to loop over a given list of elements": [
-#         "for",
-#         "while",
-#         "each",
-#         "loop",
-#     ],
-#     "What's the purpose of the built-in zip() function": [
-#         "To iterate over two or more sequences at the same time",
-#         "To combine several strings into one",
-#         "To compress several files into one archive",
-#         "To get information from the user",
-#     ],
-#     "What's the name of Python's sorting algorithm": [
-#         "Timsort",
-#         "Quicksort",
-#         "Merge sort",
-#         "Bubble sort",
-#     ],
-#     "What does dict.get(key) return if key isn't found in dict": [
-#         "None",
-#         "key",
-#         "True",
-#         "False",
-#     ],
-# }
+# QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
 
 
 def run_quiz():
     questions = prepare_questions(
-        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
     )
 
     num_correct = 0
-    for num, (question, alternatives) in enumerate(questions, start=1):
+    for num, question in enumerate(questions, start=1):
         print(f"\n*** Question {num} ***")
-        num_correct += ask_question(question, alternatives)
+        num_correct += ask_question(question)
 
     print(f"\nYou got {num_correct} out of {num} questions.")
 
 
 # preprocessing
-def prepare_questions(questions, num_questions):
+def prepare_questions(path, num_questions):
+    questions = tomllib.loads(path.read_text())["questions"]
     num_questions = min(num_questions, len(questions))
     return random.sample(list(questions.items()), k=num_questions)
 
 
-def ask_question(question, alternatives):
-    correct_answer = alternatives[0]
-    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+def ask_question(question):
+    # correct_answer = alternatives[0]
+    correct_answer = question["answer"]
+    # ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+    alternatives = question["answer"] + question["alternatives"]
+    ordered_alternatives = random.sample(
+        alternatives, k=len(alternatives)
+    )
 
-    answer = get_answer(question, ordered_alternatives)
+    answer = get_answer(question["question"], ordered_alternatives)
     point = validate_answer(correct_answer, answer)
 
     input("\nPush 'Enter' button to go to the next question.")
